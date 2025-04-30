21181d20dff470b010c447bdcd51bbd46ffcfb98
Jareem Hoff <jareem.e.hoff@gmail.com>
2025-04-30 03:41:25 -0400

    finally made some updater that should help fix some of my issues

--- main.lua
+++ main.lua
@@ -1,18 +1,25 @@
-local love = require("love")
+-- local love = require("love")
 
 local array = {}
 local maxR, maxC = 3, 3
 local x, y = 0, 0
 local w, h = 200, 200
 
-for row = 1, maxR do
-	x = x + 200
-	for col = 1, maxC do
-		y = y + 200
-		print(x)
-		print(y)
-	end
-end
-function love.draw()
-	love.graphics.text("hi", 100, 100)
+-- for row = 1, maxR do
+-- 	x = x + 200
+-- 	for col = 1, maxC do
+-- 		y = y + 200
+-- 		print(x)
+-- 		print(y)
+-- 	end
+-- end
+
+function love.load() end
+
+function love.update() end
+
+function love.draw() end
+
+function love.keypressed(key)
+	print(key)
 end

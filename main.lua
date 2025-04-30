6e730803c5fb692f8c663be8b6d3706e4155b412
12jihan <jareem.e.hoff@gmail.com>
2025-04-29 23:03:02 -0400

    starting now

--- main.lua
+++ main.lua
@@ -1,4 +1,18 @@
 local love = require("love")
+
+local array = {}
+local maxR, maxC = 3, 3
+local x, y = 0, 0
+local w, h = 200, 200
+
+for row = 1, maxR do
+	x = x + 200
+	for col = 1, maxC do
+		y = y + 200
+		print(x)
+		print(y)
+	end
+end
 function love.draw()
-	love.graphics.print("Hello World", 400, 300)
+	love.graphics.text("hi", 100, 100)
 end
